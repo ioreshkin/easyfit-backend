bccad5817d7595c2e62ea0e4ec3ed07b481fc618
ioreshkin <fennonplay@gmail.com>
2024-06-06 13:35:45 +0500

    ещкере

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
@@ -11,32 +10,28 @@ let db = new sqlite3.Database('./easyfit.db', (err) => {
 
 const hostname = '26.79.198.107';
 const port = 3002;
+const app = express();
 const jsonParser = express.json();
 
-const server = http.createServer((req, res) => {
-  if (req.method == "GET") {
-    if (req.url == "/exercises") {
-      db('SELECT * FROM exersises', [], (err, rows) => {
-        if (err) {
-          console.error(err.message);
-        }
-        let result = [];
-        // Вывод всех записей
-        rows.forEach((row) => {
-          result.push(
-            {
-              "name": row.name_exersises,
-              "description": row.discription_exersises,
-              "muscles": row.muscles_exersises
-            }
-          )
-        });
-      });
-        res.end("123");
+app.get("/exercises", (req, res) => {
+  db.all('SELECT * FROM exersises', [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
     }
-  }
+    let result = [];
+    // Вывод всех записей
+    rows.forEach((row) => {
+      result.push(
+        {
+          "name": row.name_exersises,
+          "description": row.discription_exersises,
+          "muscles": row.muscles_exersises
+        }
+      )
+    });
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.json(result)
+  });
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+app.listen(port);
