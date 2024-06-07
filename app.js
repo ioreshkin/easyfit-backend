567b193899fb5fc97c9b7131a1eb129bfa658f2e
ioreshkin <fennonplay@gmail.com>
2024-06-07 13:37:58 +0500

    some changes

--- app.js
+++ app.js
@@ -13,8 +13,85 @@ const port = 3002;
 const app = express();
 const jsonParser = express.json();
 
+const getExercises = async (sqlReq) => {
+  const result = [];
+  
+
+  await new Promise((resolve, reject) => {
+    db.all(sqlReq, [], (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        reject(err);
+        return;
+      }
+      
+      // Вывод всех записей
+      rows.forEach((row) => {
+        result.push({
+          "name": row.name_exersises,
+          "description": row.discription_exersises,
+          "muscles": row.muscles_exersises
+        });
+      });
+      resolve();
+    });
+  });
+  return result;
+};
+
 app.get("/exercises", (req, res) => {
-  db.all('SELECT * FROM exersises', [], (err, rows) => {
+  const result = getExercises("SELECT * FROM exersises")
+  result.then(arr => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.json(arr)
+  })
+  
+});
+
+app.get("/exercises/chest", (req, res) => {
+  const result = getExercises("SELECT * FROM exersises WHERE muscles_exersises = 'Грудь'")
+  result.then(arr => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.json(arr)
+  })
+});
+
+app.get("/exercises/legs", (req, res) => {
+  const result = getExercises("SELECT * FROM exersises WHERE muscles_exersises = 'Ноги'")
+  result.then(arr => {
+    console.log(arr);
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.json(arr)
+  })
+});
+
+app.get("/exercises/abs", (req, res) => {
+  const result = getExercises("SELECT * FROM exersises WHERE muscles_exersises = 'Пресс'")
+  result.then(arr => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.json(arr)
+  })
+});
+
+app.get("/exercises/arms", (req, res) => {
+  const result = getExercises("SELECT * FROM exersises WHERE muscles_exersises = 'Руки'")
+  result.then(arr => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.json(arr)
+  })
+});
+
+app.get("/exercises/back", (req, res) => {
+  const result = getExercises("SELECT * FROM exersises WHERE muscles_exersises = 'Спина'")
+  result.then(arr => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.json(arr)
+  })
+});
+
+
+app.get("/programs", (req, res) => {
+  db.all('SELECT * FROM programs', [], (err, rows) => {
     if (err) {
       console.error(err.message);
     }
@@ -23,9 +100,9 @@ app.get("/exercises", (req, res) => {
     rows.forEach((row) => {
       result.push(
         {
-          "name": row.name_exersises,
-          "description": row.discription_exersises,
-          "muscles": row.muscles_exersises
+          "name": row.name,
+          "description": row.	discription,
+          "categories": row.categories
         }
       )
     });
