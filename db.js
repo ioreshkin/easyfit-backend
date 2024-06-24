bc593386ad4734aaf2820348c6658e686d54b77b
Варвара Осока <varvara.osoka@gmail.com>
2024-06-24 19:31:08 +0500

    zxc

--- db.js
+++ db.js
@@ -8,8 +8,8 @@ const db = new sqlite3.Database('./database.sqlite', (err) => {
     
     db.run(`CREATE TABLE IF NOT EXISTS exercises (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name_ru text NOT NULL,
-	    description_ru text NOT NULL,
+        name_ru TEXT NOT NULL,
+	    description_ru TEXT NOT NULL,
         short_description_off_page_ru TEXT NOT NULL,
         short_description_on_page_ru TEXT NOT NULL,
 	    name_en	TEXT NOT NULL,
@@ -17,8 +17,8 @@ const db = new sqlite3.Database('./database.sqlite', (err) => {
         short_description_off_page_en TEXT NOT NULL,
         short_description_on_page_en TEXT NOT NULL,
 	    muscles TEXT NOT NULL,
-        preview text NOT NULL,
-        gif text NOT NULL
+        preview TEXT NOT NULL,
+        gif TEXT NOT NULL
 
     )`, (err) => {
       if (err) {
@@ -30,18 +30,18 @@ const db = new sqlite3.Database('./database.sqlite', (err) => {
 
     db.run(`CREATE TABLE IF NOT EXISTS programs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name_ru	text NOT NULL,
-	    description_ru text NOT NULL,
+        name_ru	TEXT NOT NULL,
+	    description_ru TEXT NOT NULL,
         short_description_ru TEXT NOT NULL,
-        time_ru text NOT NULL,
+        time_ru TEXT NOT NULL,
         name_en	TEXT NOT NULL,
 	    description_en TEXT NOT NULL,
 	    short_description_en TEXT NOT NULL,
-        time_en text NOT NULL,
-	    categories text NOT NULL,
-	    exercises text NOT NULL,
-        exercises_repeats text NOT NULL,
-        preview text NOT NULL
+        time_en TEXT NOT NULL,
+	    categories TEXT NOT NULL,
+	    exercises TEXT NOT NULL,
+        exercises_repeats TEXT NOT NULL,
+        preview TEXT NOT NULL
 
       )`, (err) => {
         if (err) {
